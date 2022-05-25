a05744028e7891ed1e8d154dbca072aaff4ffd52
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-05-25 13:59:10 +0000

    refactor - WIP create datastructure schema

--- src/noisy_stocks/data_structures.py
+++ src/noisy_stocks/data_structures.py
@@ -1,37 +1,68 @@
-import json
 from datetime import datetime
-from typing import Optional
+from sqlite3 import Timestamp
 
 import pandas as pd
+import pandera as pa
+from pandera.dtypes import DateTime
+from pandera.typing import DataFrame, Index, Series
 from pydantic import BaseModel
 
 # Purpose of primary datastructures:
-# TimeSeriesDict for validation of the ingested info.
 # Pandas DataFrame for analysis
 # JSON for moving information
+
+
+# Typical data order analysis:
+# 1. Fetch required timeseries aggregation from database (JSON)
+# 2. Convert timeseries into Pandas DataFrame to analyze
+# 3. Convert DataFrame to JSON for publishing
+
 
 # Classes should be PascalCase
 # Mark optional parameters by NotRequired
 
 
-class TimeSeriesDict(BaseModel):
-    # Used for validation of ingested information; Pandas has no native typing support
-    time: datetime
+class StockSchema(pa.SchemaModel):
+    timestamp: Index[DateTime] = pa.Field()
+    state: Series[str]
+    city: Series[str]
+    price: Series[int] = pa.Field(in_range={"min_value": 5, "max_value": 20})
 
 
-def time_series_dict_to_JSON(dict: TimeSeriesDict):
+format = "%Y-%M-%D"
 
-    # Exports time series dict
-    pass
+today = datetime.today()
+# data to validate
+df = pd.DataFrame(
+    {
+        "dates": [today, today, today, today, today],
+        "price_in_millions": [-1.3, -1.4, -2.9, -10.1, -20.4],
+    }
+)
+
+df["dates"] = df["dates"].dt.floor("d")  # Set time to 00:00:00 for performance
 
 
-def slice_pd_dataframe(
-    pd_dataframe: pd.DataFrame, start_date, end_date
-) -> pd.DataFrame:
+print(df)
 
-    # Slice a timerange out of a pandas dataframe
 
-    return pd_dataframe
+# define schema
+stock_schema = pa.DataFrameSchema(
+    {
+        "dates": pa.Column(Timestamp, coerce=True),
+        "price_in_millions": pa.Column(float, checks=pa.Check.lt(-1.2)),
+    },
+    strict=True,
+)
+
+validated_df = stock_schema(df)
+print(validated_df)
+
+# Test, all arrays are same length
+# Test, ValidationError
+# Test if the units are consistent
+
+# TODO: Check if there are duplicates; duplicate
 
 
 def pd_dataframe_to_JSON(pd_dataframe: pd.DataFrame):
@@ -45,6 +76,9 @@ def pd_dataframe_to_JSON(pd_dataframe: pd.DataFrame):
 
 
 class Stock(BaseModel):
-    id: str  # Stock symbol is unique identifier
-    exchange: Optional[str]
-    data: TimeSeriesDict
+    symbol: str  # Stock symbol is unique identifier
+    time_series_data: DataFrame
+
+
+def stock_to_JSON(stock: Stock):
+    return stock.json()
