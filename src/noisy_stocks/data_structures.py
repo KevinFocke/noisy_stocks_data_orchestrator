bdb5164456fabf06cefa1b41385b57f412cda11b
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-05-26 14:16:32 +0000

    refactor - include publish in egress

--- src/noisy_stocks/data_structures.py
+++ src/noisy_stocks/data_structures.py
@@ -1,10 +1,8 @@
-from datetime import datetime
-from sqlite3 import Timestamp
-
 import pandas as pd
 import pandera as pa
-from pandera.dtypes import DateTime
-from pandera.typing import DataFrame, Index, Series
+from pandera.dtypes import Timestamp
+from pandera.errors import SchemaError
+from pandera.typing import DataFrame
 from pydantic import BaseModel
 
 # Purpose of primary datastructures:
@@ -22,63 +20,59 @@ from pydantic import BaseModel
 # Mark optional parameters by NotRequired
 
 
-class StockSchema(pa.SchemaModel):
-    timestamp: Index[DateTime] = pa.Field()
-    state: Series[str]
-    city: Series[str]
-    price: Series[int] = pa.Field(in_range={"min_value": 5, "max_value": 20})
+# Load unvalidated DataFrame
 
-
-format = "%Y-%M-%D"
-
-today = datetime.today()
-# data to validate
 df = pd.DataFrame(
     {
-        "dates": [today, today, today, today, today],
-        "price_in_millions": [-1.3, -1.4, -2.9, -10.1, -20.4],
+        "dates": ["1996-10-04", "1980-02-05", "1970-02-05", "1950-02-07", "1800-02-05"],
+        "close_price": [1.3, 1.4, 0, 1, 5],
     }
 )
 
-df["dates"] = df["dates"].dt.floor("d")  # Set time to 00:00:00 for performance
+# test for dates in the future
+
+# Convert into validated DataFrame
 
 
-print(df)
+# define stock schema
 
-
-# define schema
 stock_schema = pa.DataFrameSchema(
     {
         "dates": pa.Column(Timestamp, coerce=True),
-        "price_in_millions": pa.Column(float, checks=pa.Check.lt(-1.2)),
+        "close_price": pa.Column(float, checks=pa.Check.greater_than_or_equal_to(0)),
     },
     strict=True,
 )
 
-validated_df = stock_schema(df)
-print(validated_df)
+
+def validate_df(df: pd.DataFrame):
+    """Validate the dataframe values
+
+    Args:
+        df (pd.DataFrame): Unvalidated Pandas DataFrame
+
+    Returns:
+        df (pd.DataFrame): Validated Pandas DataFrame
+    """
+
+    try:
+        validated_df = stock_schema(df)
+        return validated_df
+    except SchemaError as e:
+        print(f"{e}")  # TODO: Log error & pass this stock
+
 
 # Test, all arrays are same length
 # Test, ValidationError
 # Test if the units are consistent
+# Test except SchemaError
 
-# TODO: Check if there are duplicates; duplicate
-
-
-def pd_dataframe_to_JSON(pd_dataframe: pd.DataFrame):
-
-    # Convert dataframe to TimeSeriesDict for validation
-
-    # Use built-in method
-    # Validate
-    # Converts a Pandas DataFrame to JSON file
-    pass
+# TODO: Check if there are duplicates dates; remove them; duplicate
 
 
 class Stock(BaseModel):
     symbol: str  # Stock symbol is unique identifier
     time_series_data: DataFrame
 
-
-def stock_to_JSON(stock: Stock):
-    return stock.json()
+    def stock_to_JSON(self):
+        return self.json()
