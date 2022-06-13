f2fef8c1e1c7f82c2feaa7b04b7f76b9cd6e1d7e
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-06-13 08:02:57 +0000

    feat - add stub for secrets management

---
+++ src/noisy_stocks_data_orchestrator/secrets.py
@@ -0,0 +1,12 @@
+"""Handle secrets securely
+	"""
+
+
+# TODO: Create function that fetches secret from env variable.
+# @task
+# def retrieve_env()
+# name: str = "MYENVVARIABLE"
+# value = os.getenv(name)
+# If not in os.environ:
+# Raise ValueError
+# Retrun value
